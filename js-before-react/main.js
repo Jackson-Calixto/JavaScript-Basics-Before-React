0304370003090917130301dd6012181f4559305a
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-12 06:31:17 -0300

    Optional Chaning document.body.innerText = User.address.zip.code Cannot read properties of undefined (reading 'zip')

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -4,14 +4,14 @@ const User = {
   name: "Jackson",
   nickname: "Calixto",
   idade: 43,
-  address: {
-    street: "Rua Teste",
-    number: 176,
-    zip: {
-      code: 89245000,
-      city: "Araquari",
-    },
-  },
+  // address: {
+  //   street: "Rua Teste",
+  //   number: 176,
+  //   zip: {
+  //     code: 89245000,
+  //     city: "Araquari",
+  //   },
+  // },
 };
 
 // Optional Chaning
