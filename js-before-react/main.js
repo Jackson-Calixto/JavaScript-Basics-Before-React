3c5eb683d5a4a72b0945961cb98bbc5c2a233f9c
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-12 06:47:21 -0300

    Optional Chaning const user = {   name: "Jackson",   nickname: "Calixto",   idade: 43,   // address: {   //   street: "Rua Teste",   //   number: 176,   //   zip: {   //     code: 89245000,   //     city: "Araquari",   //   },   //   showFullAddress() {   //     return "ok";   //   },   // }, }; user.address.showFullAddress(); Uncaught TypeError: Cannot read properties of undefined (reading 'showFullAddress')

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -4,17 +4,17 @@ const user = {
   name: "Jackson",
   nickname: "Calixto",
   idade: 43,
-  address: {
-    street: "Rua Teste",
-    number: 176,
-    zip: {
-      code: 89245000,
-      city: "Araquari",
-    },
-    showFullAddress() {
-      return "ok";
-    },
-  },
+  // address: {
+  //   street: "Rua Teste",
+  //   number: 176,
+  //   zip: {
+  //     code: 89245000,
+  //     city: "Araquari",
+  //   },
+  //   showFullAddress() {
+  //     return "ok";
+  //   },
+  // },
 };
 
 // Optional Chaning
