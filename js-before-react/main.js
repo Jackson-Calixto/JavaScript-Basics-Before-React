efc9dd6a7d3adb16d08cc799e9710749dd6fcbbe
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-12 06:11:54 -0300

    Short syntax const name = 'Jackson' const idade = 43 const user = {   name:name,   idade:idade } document.body.innerText = JSON.stringify({user})

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -10,16 +10,14 @@ const User = {
   },
 };
 
-// Destructuring
+// Short syntax
 
-function mostraIdade({ idade }) {
-  return idade;
+const name = 'Jackson'
+const idade = 43
+
+const user = {
+  name:name,
+  idade:idade
 }
 
-// Rest operator
-
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const [first, , third, ...rest] = array;
-
-document.body.innerText = JSON.stringify({ first, third, rest });
+document.body.innerText = JSON.stringify({user})
