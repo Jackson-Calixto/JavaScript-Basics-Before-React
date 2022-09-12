93397fa343660859b49a870ad06eba321cf80e58
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-12 06:39:12 -0300

    Optional Chaning const user = {   name: "Jackson",   nickname: "Calixto",   idade: 43,   address: {     street: "Rua Teste",     number: 176,     //   zip: {     //     code: 89245000,     //     city: "Araquari",     //   },   }, }; user?.address?.zip?.code ?? "Não informado";

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -4,14 +4,14 @@ const user = {
   name: "Jackson",
   nickname: "Calixto",
   idade: 43,
-  // address: {
-  //   street: "Rua Teste",
-  //   number: 176,
-  //   zip: {
-  //     code: 89245000,
-  //     city: "Araquari",
-  //   },
-  // },
+  address: {
+    street: "Rua Teste",
+    number: 176,
+    //   zip: {
+    //     code: 89245000,
+    //     city: "Araquari",
+    //   },
+  },
 };
 
 // Optional Chaning
