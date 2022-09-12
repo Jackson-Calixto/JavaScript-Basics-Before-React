5f93b7fc48479913d752b90a4de05df099f35b84
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-12 06:30:13 -0300

    Optional Chaning document.body.innerText = User.address.zip.code

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -4,12 +4,16 @@ const User = {
   name: "Jackson",
   nickname: "Calixto",
   idade: 43,
-  // address: {
-  //   street: "Rua Teste",
-  //   number: 176,
-  // },
+  address: {
+    street: "Rua Teste",
+    number: 176,
+    zip: {
+      code: 89245000,
+      city: "Araquari",
+    },
+  },
 };
 
 // Optional Chaning
 
-document.body.innerText = User.address ? User.address.street : "Não infomrado";
+document.body.innerText = User.address.zip.code
