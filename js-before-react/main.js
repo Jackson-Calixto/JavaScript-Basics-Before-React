96539891bd115905bf0d0fe281ad8e2b7fcd23b8
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-12 09:54:57 -0300

    Array Methods const novoArray = array.map((item) => {   if (item % 2 === 0)     return item * 10;
      return item
    });
    document.body.innerText = JSON.stringify(novoArray);

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -3,7 +3,10 @@
 const array = [1, 2, 3, 4, 5];
 
 const novoArray = array.map((item) => {
-  return item * 2;
+  if (item % 2 === 0) 
+    return item * 10;
+  
+  return item
 });
 
 document.body.innerText = JSON.stringify(novoArray);
