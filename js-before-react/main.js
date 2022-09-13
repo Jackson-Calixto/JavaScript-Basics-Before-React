3ca58629a80aadfe22ba93550a339b9d646308bc
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-13 04:56:05 -0300

    Promises fetch cascade nested // .then / .catch
    fetch("https://api.github.com/users/Jackson-Calixto")
      .then((response) => {
        console.log(
          response.json()
            .then((body) => {
              console.log(body);
            })
        );
      })
      .catch((error) => {
        console.log(error);
      });

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -5,11 +5,8 @@ fetch("https://api.github.com/users/Jackson-Calixto")
   .then((response) => {
     console.log(
       response.json()
-        .then((responseText) => {
-          console.log(responseText);
-        })
-        .catch((err) => {
-          console.log(err);
+        .then((body) => {
+          console.log(body);
         })
     );
   })
