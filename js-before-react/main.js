1547977eeb2c0ea9043575f79bd5d11983113d4a
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-13 05:03:10 -0300

    Promises finally fetch("https://api.github.com/users/Jackson-Calixto")   .then((response) => response.json())   .then(body => console.log(body))   .catch((error) => {     console.log(error);   })   .finally(() => {     console.log('Finally...')   });

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -1,9 +1,10 @@
-// Promises fetch inline nested
-// .then / .catch
-
+// Promises finally
 fetch("https://api.github.com/users/Jackson-Calixto")
   .then((response) => response.json())
   .then(body => console.log(body))
   .catch((error) => {
     console.log(error);
+  })
+  .finally(() => {
+    console.log('Finally...')
   });
