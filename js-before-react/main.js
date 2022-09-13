2a914eb6cc60d351074fa9281a863cd9d69c5022
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-13 03:55:51 -0300

    Array Methods Reduce const soma = array.reduce((acc, item) => acc + item, 0); document.body.innerText = JSON.stringify(soma);

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -2,6 +2,6 @@
 
 const array = [1, 2, 3, 4, 5];
 
-const soma = array.reduce((acc, item) => acc + item, 10);
+const soma = array.reduce((acc, item) => acc + item, 0);
 
 document.body.innerText = JSON.stringify(soma);
