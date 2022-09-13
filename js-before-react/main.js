797af47005cb4ccd8c6d0e9328b968ab7637643c
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-13 06:06:16 -0300

    Import * as var from lib/math exported console.log(math.soma(3, 4)); console.log(math.sub(3, 4)); console.log(math.PI);

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -1,4 +1,6 @@
 // Import * as var from lib/math exported
 import * as math from "./lib/math";
 
-console.log(math)
+console.log(math.soma(3, 4));
+console.log(math.sub(3, 4));
+console.log(math.PI);
