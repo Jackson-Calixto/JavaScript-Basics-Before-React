98b0ee3dee23b528a22dbd75b38fdc262e5a6f67
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-13 06:04:48 -0300

    Import * as var from lib/math exported import * as math from "./lib/math"; console.log(math)

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -1,6 +1,4 @@
-// Had to manually add sub from lib/math exported
-import { PI, soma, sub } from "./lib/math";
+// Import * as var from lib/math exported
+import * as math from "./lib/math";
 
-console.log(soma(4, 3));
-console.log(sub(4, 3));
-console.log(PI)
+console.log(math)
