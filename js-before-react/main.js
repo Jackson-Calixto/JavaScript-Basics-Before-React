9346eff14fd8acd6765e24a5381e316685d582f8
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-09-13 05:16:44 -0300

    Promises async await trycatch async function buscaDadosGithub() {   try {     const response = await fetch(       "https://xapi.github.com/users/Jackson-Calixto"     );     const body = await response.json();     console.log(body);   } catch (error) {     console.log(error);   } }
    buscaDadosGithub();

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -1,8 +1,14 @@
-// Promises async await
+// Promises async await trycatch
 async function buscaDadosGithub() {
-  const response = await fetch("https://api.github.com/users/Jackson-Calixto");
-  const body = await response.json();
-  console.log(body);
+  try {
+    const response = await fetch(
+      "https://xapi.github.com/users/Jackson-Calixto"
+    );
+    const body = await response.json();
+    console.log(body);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 buscaDadosGithub();
